6fb9ffc5be782cb7021cfc034ab5419bb2a74046
abdelelrahmanali.mohamed <abdelrahmanali.mohamed@superhog.com>
2025-07-07 20:59:09 +0200

    implement integration test for end points

---
+++ test/TaskManagement.MinimalAPI.IntegrationTest/TasksModulesTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using TaskManagement.Application.DTO;
+
+namespace TaskManagement.MinimalAPI.IntegrationTest;
+public class TasksModulesTests : IClassFixture<TasksManagementModuleWebApplicationFactory>
+{
+	private readonly HttpClient _client;
+	public TasksModulesTests(TasksManagementModuleWebApplicationFactory factory)
+	{
+		_client = factory.CreateClient();
+	}
+
+	[Fact]
+	public async Task CreateTask_ShouldReturnOk()
+	{
+		// Arrange
+		var newTask = new CreateTaskDTO("TestTask");
+
+		// Act
+		var response = await _client.PostAsJsonAsync("/api/tasks/create", newTask);
+
+		// Assert
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+		var result = await response.Content.ReadFromJsonAsync<object>();
+		Assert.NotNull(result);
+	}
+
+	[Fact]
+	public async Task GetAllTasks_ShouldReturnOk()
+	{
+		// Act
+		var url = "/api/tasks/get-all?pageNumber=1&pageSize=10";
+		var response = await _client.GetAsync(url);
+
+		// Assert
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+		var result = await response.Content.ReadFromJsonAsync<object>();
+		Assert.NotNull(result);
+	}
+}
---
+++ test/TaskManagement.MinimalAPI.IntegrationTest/UsersModuleTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using TaskManagement.Application.DTO;
+
+namespace TaskManagement.MinimalAPI.IntegrationTest;
+public class UsersModuleTests : IClassFixture<TasksManagementModuleWebApplicationFactory>
+{
+	private readonly HttpClient _client;
+	public UsersModuleTests(TasksManagementModuleWebApplicationFactory factory)
+	{
+		_client = factory.CreateClient();
+	}
+
+	[Fact]
+	public async Task CreateUser_ShouldReturnOk()
+	{
+		// Arrange
+		var newTask = new CreateUserDTO("TestUser");
+
+		// Act
+		var response = await _client.PostAsJsonAsync("/api/users/create", newTask);
+
+		// Assert
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+		var result = await response.Content.ReadFromJsonAsync<object>();
+		Assert.NotNull(result);
+	}
+
+	[Fact]
+	public async Task GetAllUsers_ShouldReturnOk()
+	{
+		// Act
+		var url = "/api/users/get-all?pageNumber=1&pageSize=10";
+		var response = await _client.GetAsync(url);
+
+		// Assert
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+		var result = await response.Content.ReadFromJsonAsync<object>();
+		Assert.NotNull(result);
+	}
+}
